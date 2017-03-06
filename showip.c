22db80523bc5ff37e3f5d7eae6c773db8805f01d
kevinzhang334455 <kevinzhang334455@gmail.com>
2017-03-05 16:00:54 -0800

    first commit

---
+++ showip.c
@@ -0,0 +1,17 @@
+/*
+** showip.c -- show IP addresses for a host given on the command line
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
